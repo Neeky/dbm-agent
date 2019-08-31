d955de3342f6297d2d54fc4d6aac54ada259cb49
Neeky <1721900707@qq.com>
2019-08-31 22:21:26 +0800

    0.1.0 实现自动化部署相关的逻辑

---
+++ dbma/deployer.py
@@ -0,0 +1,31 @@
+"""
+自动化完成数据库安装配置这样的工作
+"""
+
+import re
+import os
+import sys
+import time
+import psutil
+import logging
+import argparse
+import requests
+import threading
+import subprocess
+import configparser
+import mysql.connector
+import logging.handlers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
