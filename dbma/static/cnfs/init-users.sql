e1a9dcb4fbd288449e5b176b0122048cf2fe5d34
Neeky <1721900707@qq.com>
2019-09-21 20:36:56 +0800

    0.1.4

--- dbma/static/cnfs/init-users.sql
+++ dbma/static/cnfs/init-users.sql
@@ -2,19 +2,26 @@ set @@sql_log_bin=0;
 
 alter user root@'localhost' identified by 'dbma@0352';
 create user root@'127.0.0.1' identified by 'dbma@0352';
-grant all on *.* to root@'127.0.0.1' ;
+grant all on *.* to root@'127.0.0.1' with grant option;
 
 create user dbma@'127.0.0.1' identified by 'dbma@0352';
-grant all on *.* to dbma@'127.0.0.1';
+grant all on *.* to dbma@'127.0.0.1' with grant option;
 
-create user 'clone_user'@'127.0.0.1' identified by 'dbma@0352';
-grant clone_admin on *.* to 'clone_user'@'127.0.0.1';
-create user 'clone_user'@'localhost' identified by 'dbma@0352';
-grant clone_admin on *.* to 'clone_user'@'localhost';
+-- clone
+create user 'cloneuser'@'127.0.0.1' identified by 'dbma@0352';
+grant clone_admin on *.* to 'cloneuser'@'127.0.0.1';
+create user 'cloneuser'@'localhost' identified by 'dbma@0352';
+grant clone_admin on *.* to 'cloneuser'@'localhost';
+create user 'cloneuser'@'%' identified by 'dbma@0352';
+grant backup_admin on *.* to 'cloneuser'@'%';
 
-create user 'clone_user'@'%' identified by 'dbma@0352';
-grant backup_admin on *.* to 'clone_user'@'%';
+-- replication
+create user 'repluser'@'%' identified by 'dbma@0352';
+grant replication slave,replication client on *.* to 'repluser'@'%';
 
+-- monitor
+create user monitor@'127.0.0.1' identified by 'dbma@0352';
+grant replication client,process on *.* to monitor@'127.0.0.1';
 
 set @@sql_log_bin=1;
 
