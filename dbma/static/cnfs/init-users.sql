84a2df2a9ecd9fe6db86f0a9a617e3afa4ffcb34
Neeky <1721900707@qq.com>
2019-09-21 15:37:01 +0800

    0.1.4

--- dbma/static/cnfs/init-users.sql
+++ dbma/static/cnfs/init-users.sql
@@ -1,15 +1,19 @@
 set @@sql_log_bin=0;
 
-create user dbma@'127.0.0.1' identified by 'dbma@0352';
-grant all on *.* to dbma@'127.0.0.1';
-
 alter user root@'localhost' identified by 'dbma@0352';
-
 create user root@'127.0.0.1' identified by 'dbma@0352';
 grant all on *.* to root@'127.0.0.1' ;
 
+create user dbma@'127.0.0.1' identified by 'dbma@0352';
+grant all on *.* to dbma@'127.0.0.1';
+
 create user 'clone_user'@'127.0.0.1' identified by 'dbma@0352';
-grant backup_admin on *.* to 'clone_user'@'127.0.0.1';
+grant clone_admin on *.* to 'clone_user'@'127.0.0.1';
+create user 'clone_user'@'localhost' identified by 'dbma@0352';
+grant clone_admin on *.* to 'clone_user'@'localhost';
+
+create user 'clone_user'@'%' identified by 'dbma@0352';
+grant backup_admin on *.* to 'clone_user'@'%';
 
 
 set @@sql_log_bin=1;
