8df1a8211e95a1009785fd5ef87023c8a742f51b
Neeky <1721900707@qq.com>
2019-09-29 13:41:34 +0800

    0.1.9 添加更多基础监控

--- tests/test_gather.py
+++ tests/test_gather.py
@@ -10,10 +10,12 @@ class GatherTestCase(unittest.TestCase):
     """
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
         pass
 
     @classmethod
     def tearDownClass(cls):
+        super().setUpClass()
         pass
 
     def test_01_cpu_cores(self):
@@ -24,6 +26,19 @@ class GatherTestCase(unittest.TestCase):
         self.assertIsInstance(gather.cpu_cores(),gather.CpuCores)
         self.assertIsInstance(gather.cpu_cores().counts,int)
     
+    def test_02_cpu_times(self):
+        """
+        测试 cpu_times
+        """
+        self.assertIsInstance(gather.cpu_times(),gather.CpuTimes)
+    
+    def test_02_cpu_frequence(self):
+        """
+        测试 cpu_frequence
+        """
+        self.assertIsInstance(gather.cpu_frequence,gather.CpuFrequency)
+    
+
 
 
 
