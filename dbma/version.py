22f4d0aeca13b44c595b037b40d4d64c347450d5
Neeky <1721900707@qq.com>
2019-10-18 09:24:49 +0800

    0.2.5

--- dbma/version.py
+++ dbma/version.py
@@ -1,1 +1,1 @@
-agent_version = '0.2.4'
+agent_version = '0.2.5'
---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+from dbma.version import agent_version
+import re
+import unittest
+
+class VersionTestCase(unittest.TestCase):
+    """
+    验证 version 模块中的 agent_vesion 格式上是否正确
+    """
+    def test_01_version(self):
+        self.assertIsInstance(agent_version,str)
+
+        m = re.search(r'\d.\d.\d',agent_version)
+        self.assertIsNotNone(m)
+
