36024b4199c4541aa0de1e40799902cb92967317
Neeky <1721900707@qq.com>
2020-01-07 15:25:53 +0800

    add-messages

---
+++ dbma/messages.py
@@ -0,0 +1,14 @@
+"""
+定义若干常量
+"""
+# (c) 2019, LeXing Jiang <neeky@live.com 1721900707@qq.com https://www.sqlpy.com/>
+# Copyright: (c) 2019, dbm Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# -- 用户相关 --
+
+# 用户不存在
+SYS_USER_NOT_EXISTS = f"system user '{0}' not exists"
+
+# 用户已经存在
+SYS_USER_ALREADY_EXISTS = f"system user '{0}' alread exists"
