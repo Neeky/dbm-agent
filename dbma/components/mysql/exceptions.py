26aba8fe9c0e54f817b2b68c8fa5b60911d6eeb7
Neeky <1721900707@qq.com>
2023-03-29 16:14:35 +0800

    ✨feature: 业务异常树

---
+++ dbma/components/mysql/exceptions.py
@@ -0,0 +1,21 @@
+from dbma.core.exception import DBMAgentException
+
+class MySQLTemplateFileNotExistsException(DBMAgentException):
+    """MySQL 配置文件模板不存在
+    """
+    pass
+
+class MySQLPkgFileNotExistsException(DBMAgentException):
+    """MySQL 安装包文件模板不存在
+    """
+    pass
+
+class InstanceHasBeenInstalledException(DBMAgentException):
+    """给定实例已经安装过了
+    """
+    pass
+
+class MySQLSystemdFileNotExists(DBMAgentException):
+    """
+    """
+    pass
---
+++ dbma/core/exception.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+
+"""dbm-agent exceptions
+
+"""
+
+class DBMAgentException(Exception):
+    pass
