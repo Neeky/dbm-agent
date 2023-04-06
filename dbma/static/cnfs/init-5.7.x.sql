5c9e2d6fd7ca8226e6fd6264a5031dbe030038e7
Neeky <1721900707@qq.com>
2023-04-06 17:24:22 +0800

    ✨feature: 实现初始化备机的功能

--- dbma/static/cnfs/init-5.7.x.sql
+++ dbma/static/cnfs/init-5.7.x.sql
@@ -8,4 +8,8 @@ grant all on *.* to root@'127.0.0.1' with grant option;
 create user dbma@'127.0.0.1' identified by 'dbma@0352';
 grant all on *.* to dbma@'127.0.0.1' with grant option;
 
+-- 创建复制用户
+create user repl@'%' identified by '2-4nw9A0-459st36';
+grant replication slave, replication client on *.* to repl@'%';
+
 set @@sql_log_bin=1;
