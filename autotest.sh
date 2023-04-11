607cc566373d70054883ada1092f7d301391fa62
Neeky <1721900707@qq.com>
2023-04-11 11:51:58 +0800

    ✨feature: 增加测试用到的依赖信息

--- autotest.sh
+++ autotest.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-#自动发现所有的测试用例
-# 生成 html 报表
+
+
+#--------------------------------------------#
+# pip3 install pytest pytest-mock pytest-cov |
+#--------------------------------------------#
+
+
+rm -rf ./htmlcov/*
 pytest --cov --cov-report=html
 
 # 生成字符报表
