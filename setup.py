6323ef211a741a3d851d69cd428bff7ccacc8b3d
Neeky <1721900707@qq.com>
2023-04-11 11:21:57 +0800

    ✨feature:

--- setup.py
+++ setup.py
@@ -15,36 +15,56 @@ def get_version():
     g = {}
 
     exec(content, g, g)
-    return g['VERSION']
+    return g["VERSION"]
 
 
 agent_version = get_version()
 
-setup(name='dbm-agent',
-      version=agent_version,
-      description='dbm-agent 数据库管理中心客户端程序',
-      author="Neeky",
-      author_email="neeky@live.com",
-      maintainer='Neeky',
-      maintainer_email='neeky@live.com',
-      scripts=['bin/dbm-agent', 'bin/dbma-cli-single-instance', 'bin/dbma-cli-init',
-               'bin/dbm-bt-conn-stack'],
-      packages=['dbma', 'dbma/core', 'dbma/core/views', 'dbma/core/agent', 'dbma/core/threads',
-                'dbma/bil', 'dbma/components', 'dbma/components/mysql', 'dbma/components/redis',
-                'dbma/components/mysql/views'],
-      package_data={'dbma': ['static/cnfs/*', 'static/sql-scripts/*']},
-      url='https://github.com/Neeky/dbm-agent',
-      install_requires=['Jinja2>=2.10.1', 'mysql-connector-python>=8.0.31',
-                        'psutil>=5.6.6', 'requests>=2.22.0', 'distro>=1.4.0',
-                        'aiohttp>=3.8.1'],
-      python_requires='>=3.8.*',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Operating System :: POSIX',
-          'Operating System :: MacOS :: MacOS X',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11']
-      )
+setup(
+    name="dbm-agent",
+    version=agent_version,
+    description="dbm-agent 数据库管理中心客户端程序",
+    author="Neeky",
+    author_email="neeky@live.com",
+    maintainer="Neeky",
+    maintainer_email="neeky@live.com",
+    scripts=[
+        "bin/dbm-agent",
+        "bin/dbma-cli-single-instance",
+        "bin/dbma-cli-init",
+        "bin/dbm-bt-conn-stack",
+    ],
+    packages=[
+        "dbma",
+        "dbma/core",
+        "dbma/core/views",
+        "dbma/core/agent",
+        "dbma/core/threads",
+        "dbma/bil",
+        "dbma/components",
+        "dbma/components/mysql",
+        "dbma/components/redis",
+        "dbma/components/mysql/views",
+    ],
+    package_data={"dbma": ["static/cnfs/*", "static/sql-scripts/*"]},
+    url="https://github.com/Neeky/dbm-agent",
+    install_requires=[
+        "Jinja2>=2.10.1",
+        "mysql-connector-python>=8.0.31",
+        "psutil>=5.6.6",
+        "requests>=2.22.0",
+        "distro>=1.4.0",
+        "aiohttp>=3.8.1",
+    ],
+    python_requires=">=3.8.*",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
