8aefa4094bf9b94ce51e76e153e5870432cb0c73
Neeky <1721900707@qq.com>
2023-04-23 11:57:07 +0800

    ✨feature: 初始化的时候要创建 xtrabackup 用户

--- dbma/static/cnfs/init-8.0.x.sql
+++ dbma/static/cnfs/init-8.0.x.sql
@@ -12,4 +12,18 @@ grant all on *.* to dbma@'127.0.0.1' with grant option;
 create user repl@'%' identified by '2-4nw9A0-459st36';
 grant replication slave, replication client on *.* to repl@'%';
 
+-- xtrabackup
+create user xtrabackup@'127.0.0.1' identified by 'dbma@0352';
+grant backup_admin, process, reload, lock tables, replication client on *.* to xtrabackup@'127.0.0.1';
+grant select on performance_schema.log_status to xtrabackup@'127.0.0.1';
+grant select on performance_schema.keyring_component_status to xtrabackup@'127.0.0.1';
+grant select on performance_schema.replication_group_members to xtrabackup@'127.0.0.1';
+
+-- xtrabackup
+create user xtrabackup@'localhost' identified by 'dbma@0352';
+grant backup_admin, process, reload, lock tables, replication client on *.* to xtrabackup@'localhost';
+grant select on performance_schema.log_status to xtrabackup@'localhost';
+grant select on performance_schema.keyring_component_status to xtrabackup@'localhost';
+grant select on performance_schema.replication_group_members to xtrabackup@'localhost';
+
 set @@sql_log_bin=1;
