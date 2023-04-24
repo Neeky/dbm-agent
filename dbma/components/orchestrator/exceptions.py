d284b2aeed7721b9fd92a38c2a7f7c34bef6ffae
Neeky <1721900707@qq.com>
2023-04-24 19:54:05 +0800

    ✨feature: 增加 orch 相关的异常

---
+++ dbma/components/orchestrator/exceptions.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+"""orchestrator 组件用到的异常对象
+"""
+
+from dbma.core.exception import DBMAgentException
+
+
+class OrchHasBeenInstalledException(DBMAgentException):
+    """orch 已经安装过了"""
+
+    pass
+
+
+class OrchPkgNotExistsException(DBMAgentException):
+    """orch 安装包不存在"""
+
+    pass
