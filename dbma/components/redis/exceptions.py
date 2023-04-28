478be9c607a42253a65d271b990fc62e67e1a008
Neeky <1721900707@qq.com>
2023-04-28 14:27:53 +0800

    ✨feature: Redis 相关的异常

--- dbma/components/redis/exceptions.py
+++ dbma/components/redis/exceptions.py
@@ -2,10 +2,20 @@
 
 """Resdis 相关的异常"""
 
-from dbma.core.exception import FileExistsException, FileNotExistsException
+from dbma.core.exception import (
+    FileExistsException,
+    FileNotExistsException,
+    DirectoryExistsException,
+)
 
 
 class RedisConfigTemplateFileNotExistsException(FileNotExistsException):
     """Redis 配置文件模板不存在"""
+
+    pass
+
+
+class RedisDatabaseDirectoryExistsException(DirectoryExistsException):
+    """Redis 的数据目录已经存在"""
 
     pass
