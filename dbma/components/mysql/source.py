09688f2c95f0ec0c2925b160519184402b0ccedb
Neeky <1721900707@qq.com>
2023-04-28 17:02:00 +0800

    ✨feature: 补充注释

--- dbma/components/mysql/source.py
+++ dbma/components/mysql/source.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-"""MySQL 备机相关的操作
+"""MySQL 源实例相关的操作
 
 作者: 蒋乐兴|neeky@live.com
 时间: 2023-04
@@ -21,17 +21,22 @@ def install_mysql_source(
 ):
     """安装 MySQL Source 结点
 
-    Parameters:
-    -----------
-
+    Parameters
+    ----------
+    port : int, optional
+        MySQL 的端口号, by default 3306
+    pkg : Path, optional
+        安装包的全路径, by default default_pkg
+    innodb_buffer_pool_size : str, optional
+        innodb buffer pool 的大小, by default "128M"
     """
-    #
+    # 安装 MySQL 实例
     logging.info(messages.FUN_STARTS.format(fname()))
     install_mysql(port, pkg, innodb_buffer_pool_size=innodb_buffer_pool_size)
 
-    # sleep
+    # 把只读打开
     time.sleep(7)
     make_mysql_writable(port)
 
-    #
+    # 返回
     logging.info(messages.FUN_ENDS.format(fname()))
