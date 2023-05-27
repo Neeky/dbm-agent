e5a1ae314afc6a6e3d59ab32fedac6ef39cd5d51
Neeky <1721900707@qq.com>
2023-05-27 20:45:50 +0800

    ✨feature: 修复 item 的污染问题

--- dbma/core/agent/init.py
+++ dbma/core/agent/init.py
@@ -73,7 +73,7 @@ def create_dbm_agent_and_database_directorys():
     # 创建 MySQL+Redis 会用的的一些公共目录
     DATABASE_DIR = Path("/database")
     if not DATABASE_DIR.exists():
-        item.mkdir()
+        DATABASE_DIR.mkdir()
 
     for subdir in ("mysql", "mysql/data", "mysql/binlog", "redis"):
         item = DATABASE_DIR / subdir
