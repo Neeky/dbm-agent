de085432826d87c22ddc14d872ff5ccd3ee20db1
Neeky <1721900707@qq.com>
2023-06-16 21:47:10 +0800

    ✨feature: 增加对 create_user_and_dirs 的测试

--- tests/components/mysql/test_install.py
+++ tests/components/mysql/test_install.py
@@ -10,7 +10,7 @@ from dbma.components.mysql.install import (
     enable_systemd_for_mysql,
     disable_systemd_for_mysql,
     check_mysql_systemd_exists,
-    exe_shell_cmd,
+    create_user_and_dirs,
     start_mysql,
     stop_mysql,
 )
@@ -21,6 +21,7 @@ from dbma.components.mysql.asserts import (
     assert_mysql_systemd_file_not_exists,
 )
 from dbma.components.mysql.exceptions import MySQLSystemdFileNotExists
+from dbma.bil.osuser import MySQLUser
 
 
 # region create_init_sql_file
@@ -211,6 +212,7 @@ class DisableSystemdForMysqlTestCase(unittest.TestCase):
 
 # endregion disable_systemd_for_mysql
 
+
 # region check_mysql_systemd_exists
 
 
@@ -330,3 +332,85 @@ class StopMySQLTestCase(unittest.TestCase):
 
 
 # endregion stop_mysql
+
+
+# region create_user_and_dirs
+class CreateUserAndDirsTestCase(unittest.TestCase):
+    """ """
+
+    port = 3306
+
+    @patch("os.mkdir")
+    @patch("dbma.components.mysql.install.Path")
+    @patch("dbma.components.mysql.install.MySQLUser")
+    def test_create_user_and_dirs_given_user_and_path_not_exists(
+        self, mock_mysql_user_cls, mock_path_cls, mock_os_mkdir
+    ):
+        """
+        given: 给定的用户+路径都不存在
+        when: 调用 create_user_and_dirs
+        then: 创建相关的目录与用户
+        """
+        user = Mock()
+        mock_mysql_user_cls.return_value = user
+
+        def return_self(self, other):
+            return self
+
+        # 让 mock 对象支持除法，这里还有些问题，测试代码太 hack 了，后面要重写业务逻辑
+        path = Mock()
+        path.__class__.__truediv__ = return_self
+        mock_path_cls.return_value = path
+        path.exists.return_value = False
+
+        create_user_and_dirs()
+
+        # 调用 create
+        user.create.assert_called_once()
+        user.create.assert_called_once_with()
+
+        # exists 调用两次
+        self.assertEqual(path.exists.call_count, 2)
+
+        # 由于 datadir + binlogdir 都不存在，所以 os.mkdir 要被调用两次, chown 也会把调用两次
+        self.assertEqual(mock_os_mkdir.call_count, 2)
+        self.assertEqual(user.chown.call_count, 2)
+
+    @patch("os.mkdir")
+    @patch("dbma.components.mysql.install.Path")
+    @patch("dbma.components.mysql.install.MySQLUser")
+    def test_create_user_and_dirs_given_user_and_path_exists(
+        self, mock_mysql_user_cls, mock_path_cls, mock_os_mkdir
+    ):
+        """
+        given: 给定的用户+路径都存在
+        when: 调用 create_user_and_dirs
+        then: 除了检查基本不做事
+        """
+        user = Mock()
+        mock_mysql_user_cls.return_value = user
+
+        def return_self(self, other):
+            return self
+
+        # 让 mock 对象支持除法，这里还有些问题，测试代码太 hack 了，后面要重写业务逻辑
+        path = Mock()
+        path.__class__.__truediv__ = return_self
+        mock_path_cls.return_value = path
+        path.exists.return_value = True
+
+        create_user_and_dirs()
+
+        # 调用 create
+        user.create.assert_called_once()
+        user.create.assert_called_once_with()
+
+        # exists 调用两次
+        self.assertEqual(path.exists.call_count, 2)
+
+        # 由于 datadir + binlogdir 都不存在，所以 os.mkdir 要被调用两次, chown 也会把调用两次
+        self.assertEqual(mock_os_mkdir.call_count, 0)
+        self.assertEqual(user.chown.call_count, 2)
+
+
+# endregion create_user_and_dirs
