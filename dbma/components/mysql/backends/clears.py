15dc22ebd8a4663916db81b318163fe37b7c22f4
Neeky <1721900707@qq.com>
2023-06-18 22:10:44 +0800

    ✨feature: 支持备份过期计算

--- dbma/components/mysql/backends/clears.py
+++ dbma/components/mysql/backends/clears.py
@@ -16,23 +16,14 @@
 
 import re
 import glob
+import logging
 from pathlib import Path
+from datetime import datetime
 from collections import deque
 from dataclasses import dataclass
+from dbma.bil.fun import fname
+from dbma.core import messages
 from dbma.core.configs import dbm_agent_config
-
-
-@dataclass
-class ClearTask(object):
-    """
-    清理任务对象
-    """
-
-    # is_cnf_cleared: bool
-    # is_binlog_cleared: bool
-    # is_data_cleared: bool
-
-    dir_path: Path
 
 
 clear_tasks = deque(maxlen=1024)
@@ -42,33 +33,69 @@ backup_dir_re_pattern = re.compile(
 )
 
 
+@dataclass
+class ClearTask(object):
+    """
+    清理任务对象
+    """
+
+    path: Path
+
+    def rename_at(self):
+        """根据目录名，计算出目录备份的时间点"""
+        # 目录的后缀是 “2023-05-29T20-23-32-472128” 这个格式，也就是说它不是一个正确的时间日期格式
+        # 如果我们要得到 datetime 对象，还要给它整一下才行
+        match = backup_dir_re_pattern.search(str(self.path))
+        dt_str = match.group("dt")
+        date_str, tm_str = dt_str.split("T")
+        hour, minte, seconde, ms = tm_str.split("-")
+        datetime_str = "{} {}:{}:{}.{}".format(date_str, hour, minte, seconde, ms)
+        return datetime.fromisoformat(datetime_str)
+
+    def is_expired(self, now=None):
+        """
+        检查 rename 文件是否已经过期了
+        """
+        match = backup_dir_re_pattern.search(str(self.path))
+        # 用传好格式构造 datetime 对象
+        if match:
+            now = datetime.now() if now is None else now
+            delta = now - self.rename_at()
+            if delta.days >= 3:
+                return True
+        # 没有匹配到正则、或是没有超过 3 天
+        return False
+
+
 def scan_data_dir_gen_task():
     """只扫描 binlog 目录和 data 目录；至于 backup 目录这个是交由备份子系统完成
 
     Returns:
     --------
-    ClearTask
+    [ClearTask,ClearTask ...]
     """
-    result = []
-    # 检查 data 目录下的备份
-    data_dir_path = (
-        dbm_agent_config.mysql_datadir_parent + "*"
-        if dbm_agent_config.mysql_datadir_parent.endswith("/")
-        else dbm_agent_config.mysql_datadir_parent + "/*"
-    )
-    for instance_path in glob.glob(data_dir_path):
-        match = backup_dir_re_pattern.search(instance_path)
-        if match:
-            result.append(instance_path)
+    logging.info(messages.FUN_STARTS.format(fname()))
 
-    # 检查 binlog 目录下的备份
-    binlog_dir_path = (
-        dbm_agent_config.mysql_binlogdir_parent + "*"
-        if dbm_agent_config.mysql_binlogdir_parent.endswith("/")
-        else dbm_agent_config.mysql_binlogdir_parent + "/*"
-    )
-    for binlog_path in glob.glob(binlog_dir_path):
-        match = backup_dir_re_pattern.search(binlog_path)
-        if match:
-            result.append(binlog_path)
+    result = []
+    target_dirs = [
+        dbm_agent_config.mysql_datadir_parent,
+        dbm_agent_config.mysql_binlogdir_parent,
+    ]
+    for path in target_dirs:
+        # 处理一下路径的格式，让它可以满足 glob.glob 的要求
+        if path.endswith("/"):
+            target = path + "*"
+        else:
+            target = path + "/*"
+
+        logging.info("scan dir '{}' .".format(target))
+
+        # 逐个比较找到 ${port}-backup-xxx 格式的备份目录
+        for instance_path in glob.glob(target):
+            if backup_dir_re_pattern.search(instance_path):
+                logging.info("find '{}' .".format(instance_path))
+                # 构造 ClearTask 对象
+                result.append(ClearTask(instance_path))
+
+    logging.info(messages.FUN_ENDS.format(fname()))
     return result
--- tests/components/mysql/backends/test_clears.py
+++ tests/components/mysql/backends/test_clears.py
@@ -3,15 +3,21 @@
 import unittest
 from unittest.mock import Mock, patch, MagicMock, call
 from pathlib import Path
+from datetime import datetime
 from dbma.components.mysql.backends.clears import ClearTask, scan_data_dir_gen_task
 
 
+# region scan_data_dir_gen_task
 class ScanDirsGenTaskTestCase(unittest.TestCase):
     """ """
 
     @patch("glob.glob")
     def test_scan_data_dir_gen_task(self, mock):
-        """ """
+        """
+        given: 预期的调用环境下
+        when: 调用 scan_data_dir_gen_task
+        then: /database/mysql/data/*, /database/mysql/binlog/* 这两个地方都要被扫一次
+        """
         mock.return_value = [
             "/database/mysql/data/3308-backup-2023-05-29T20-23-32-472128",
             "/database/mysql/data/3307-backup-2023-06-17T14-45-21-949788",
@@ -21,3 +27,34 @@ class ScanDirsGenTaskTestCase(unittest.TestCase):
 
         calls = [call("/database/mysql/data/*"), call("/database/mysql/binlog/*")]
         self.assertEqual(mock.call_args_list, calls)
+
+
+# endregion scan_data_dir_gen_task
+
+
+# region ClearTask
+class ClearTaskTestCase(unittest.TestCase):
+    def test__init(self):
+        task = ClearTask("/database/mysql/data/3308-backup-2023-05-29T20-23-32-472128")
+
+    def test_is_expired_given_dir_expired(self):
+        """
+        given: 给定的备份目录已经过期
+        when: 调用 task.is_expired()
+        then: 返回 True
+        """
+        task = ClearTask("/database/mysql/data/3308-backup-2023-05-29T20-23-32-472128")
+        self.assertEqual(task.is_expired(), True)
+
+    def test_is_expired_given_dir_not_expired(self):
+        """
+        given: 给定的备份目录没有过期
+        when: 调用 task.is_expired()
+        then: 返回 False
+        """
+        task = ClearTask("/database/mysql/data/3308-backup-2023-01-01T01-01-01-472128")
+        now = datetime(2023, 1, 2, 0, 0, 0)
+        self.assertEqual(task.is_expired(now), False)
+
+
+# endregion ClearTask
