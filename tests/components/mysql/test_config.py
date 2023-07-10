4642b2babcd6fe8981bb7e511beacfd2fec5dd02
Neeky <1721900707@qq.com>
2023-07-10 20:52:41 +0800

    ✨feature: 补充测试用例

--- tests/components/mysql/test_config.py
+++ tests/components/mysql/test_config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 import unittest
-from unittest.mock import Mock, patch, MagicMock, call
+from unittest.mock import Mock, patch, MagicMock, call, mock_open
 from pathlib import Path
 
 from dbma.components.mysql.config import MySQLConfig
@@ -16,18 +16,38 @@ class MySQLConfigTestCase(unittest.TestCase):
     port = 3333
     innodb_buffer_pool_size = "128M"
 
-    def test_given_(self):
-        """ """
+    def test_version_given_an_normal_cnf(self):
+        """
+        given: 创建一个 MySQLConfig 的实例
+        then: 它的 version 属性值将与 basedir 中的版本号保持一致
+        """
         cnf = MySQLConfig(self.basedir, self.port, self.innodb_buffer_pool_size)
         self.assertEqual(cnf.version, "8.0.33")
-        cnf._calcu_deps_basedir = Mock()
-        cnf._calcu_deps_mem = Mock()
-        cnf._calcu_deps_port = Mock()
-        cnf._calcu_random_attrs = Mock()
 
-        # cnf.calcu_second_attrs()
+    @patch.object(Path, "exists")
+    def test_save_to_target_dir_given_dir_exists(self, mock_exists):
+        """
+        given: 给定一个 MySQLConfig 的实例, 给定一个存在的目录
+        when: 调用它的 save_to_target_dir 函数
+        then: 会把配置项以 json 的形式，保存到文件中
+        """
+        mock_exists.return_value = True
+        cnf = MySQLConfig(self.basedir, self.port, self.innodb_buffer_pool_size)
+        with patch("dbma.components.mysql.config.open", mock_open(read_data="x")) as m:
+            cnf.save_to_target_dir(Path("/tmp/"))
+            # 断言它有以写的方式打开文件
+            m.assert_called_with(Path("/tmp/mysql-config.json"), "w")
 
-        # cnf._calcu_deps_basedir.assert_called_once()
-        # cnf._calcu_deps_mem.assert_called_once()
-        # cnf._calcu_deps_port.assert_called_once()
-        # cnf._calcu_random_attrs.assert_called_once()
+    @patch.object(Path, "exists")
+    def test_save_to_target_dir_given_dir_not_exists(self, mock_exists):
+        """
+        given: 给定一个 MySQLConfig 的实例, 给定一个不存在的目录
+        when: 调用它的 save_to_target_dir 函数
+        then: 不会打开任命文件进行写入操作
+        """
+        mock_exists.return_value = False
+        cnf = MySQLConfig(self.basedir, self.port, self.innodb_buffer_pool_size)
+        with patch("dbma.components.mysql.config.open", mock_open(read_data="x")) as m:
+            cnf.save_to_target_dir(Path("/tmp/"))
+            # 断言它有以写的方式打开文件
+            m.assert_not_called()
